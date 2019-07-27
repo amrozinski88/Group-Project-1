66bf0c5750686bd9e408050e28376ea84f9c72e2
Justin <quezada@Justins-MacBook-Pro.local>
2019-07-27 10:23:03 -0400

    added function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,36 @@
-console.log("im working")
+$(document).ready(function(){
+
+
+console.log("we good")
+function searchBandsInTown(artist) {
+console.log(artist)
+var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+
+      console.log(response);
+
+      var artistName = $("<p>").text(response.name);
+      var artistURL = $("<a>").attr("href", response.url).append(artistName);
+      var artistImage = $("<img>").attr("src", response.thumb_url);
+      var trackerCount = $("<p>").text(response.tracker_count + " fans tracking this artist");
+      var upcomingEvents = $("<p>").text(response.upcoming_event_count + " upcoming events");
+
+      $("#artist-info").empty();
+      $("#artist-info").append(artistURL, artistImage, trackerCount, upcomingEvents);
+    });
+  }
+  // button
+  $("#searchBtn").on("click", function(event) {
+    console.log("click")
+    // Preventing the button from trying to submit the form
+    event.preventDefault();
+    // textbox
+    var inputArtist = $("#search").val().trim();
+
+    // Running the searchBandsInTown function(passing in the artist as an argument)
+    searchBandsInTown(inputArtist);
+  });
+}) 
