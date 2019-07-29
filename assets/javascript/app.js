e7a1d0065ad0d82e9e4b4fe926ca82a16af9ae46
AdamM <ajmrozinski@gmail.com>
2019-07-29 01:35:56 -0400

    index.html:moved js scripts to bottom of body and added popper/bootstrap,added div for videos js| app.js: created function for pulling youtube video on seach btn click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,47 @@
 console.log("im working")
+
+
+function getVideo(query) {
+  var queryURL = "https://www.googleapis.com/youtube/v3/search?part=id&maxResults=1&q=" + query + "&type=video&key=AIzaSyCzq7GrG1jBxY4R6yJF828EGxv8uHZVTnc";
+  var videoURL = "";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response)
+    var videoId = response.items[0].id.videoId
+    videoURL = "https://www.youtube.com/embed/" + videoId;
+    console.log(videoURL);
+    var videoIframe = $("<iframe>");
+    videoIframe.attr("src",videoURL);
+    videoIframe.attr("height","316");
+    videoIframe.attr("width","560");
+    videoIframe.attr("frameborder","0");
+    videoIframe.attr("allow","accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture");
+    videoIframe.attr("allowfullscreen");
+    $("#videoSection").html(videoIframe);
+
+
+
+
+    
+
+  })
+}
+$("#searchBtn").on("click", function (event) {
+  event.preventDefault();
+  var temp = $("#search").val().trim()
+  console.log(temp)
+  console.log("yes")
+
+  getVideo(temp);
+
+
+
+
+})
+
+
+
+
