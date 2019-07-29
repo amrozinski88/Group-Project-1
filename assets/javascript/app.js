e9220d21e97d734c57664225eaba60f996eda8bd
Lauren <ljaimes92@gmail.com>
2019-07-29 19:18:36 -0400

    empty place holders on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,39 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
 
 
-console.log("we good")
-// Function used to get video and append to videoSection div on search
-function getVideo(query) {
-  var queryURL = "https://www.googleapis.com/youtube/v3/search?part=id&maxResults=1&q=" + query + "&type=video&key=AIzaSyCzq7GrG1jBxY4R6yJF828EGxv8uHZVTnc";
-  var videoURL = "";
-// Calling ajax 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-    var videoId = response.items[0].id.videoId
-    videoURL = "https://www.youtube.com/embed/" + videoId;
-    // creating iframe element to fill with searched video
-    var videoIframe = $("<iframe>");
-    videoIframe.attr("src", videoURL);
-    videoIframe.attr("height", "316");
-    videoIframe.attr("width", "560");
-    videoIframe.attr("frameborder", "0");
-    videoIframe.attr("allow", "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture");
-    videoIframe.attr("allowfullscreen");
-    $("#videoSection").html(videoIframe);
-  })
-}
-
-
-function searchBandsInTown(artist) {
-console.log(artist)
-var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+  console.log("we good")
+  // Function used to get video and append to videoSection div on search
+  function getVideo(query) {
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=id&maxResults=1&q=" + query + "&type=video&key=AIzaSyCzq7GrG1jBxY4R6yJF828EGxv8uHZVTnc";
+    var videoURL = "";
+    // Calling ajax 
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
+      var videoId = response.items[0].id.videoId
+      videoURL = "https://www.youtube.com/embed/" + videoId;
+      // creating iframe element to fill with searched video
+      var videoIframe = $("<iframe>");
+      videoIframe.attr("src", videoURL);
+      videoIframe.attr("height", "316");
+      videoIframe.attr("width", "560");
+      videoIframe.attr("frameborder", "0");
+      videoIframe.attr("allow", "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture");
+      videoIframe.attr("allowfullscreen");
+      $("#videoSection").html(videoIframe);
+    })
+  }
+
+
+  function searchBandsInTown(artist) {
+    console.log(artist)
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
 
       console.log(response);
 
@@ -48,8 +48,10 @@ var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codin
     });
   }
   // button
-  $("#searchBtn").on("click", function(event) {
+  $("#searchBtn").on("click", function (event) {
     console.log("click")
+    // empty place holders
+    $(".card-text").empty();
     // Preventing the button from trying to submit the form
     event.preventDefault();
     // textbox
