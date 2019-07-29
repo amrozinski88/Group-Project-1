3d8615f39e1f54c7393369089c4783da861447f3
AdamM <ajmrozinski@gmail.com>
2019-07-29 01:50:45 -0400

    cleaned up code and wrote comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,31 @@
-console.log("im working")
 
-
+// Function used to get video and append to videoSection div on search
 function getVideo(query) {
   var queryURL = "https://www.googleapis.com/youtube/v3/search?part=id&maxResults=1&q=" + query + "&type=video&key=AIzaSyCzq7GrG1jBxY4R6yJF828EGxv8uHZVTnc";
   var videoURL = "";
-
+// Calling ajax 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-    console.log(response)
     var videoId = response.items[0].id.videoId
     videoURL = "https://www.youtube.com/embed/" + videoId;
-    console.log(videoURL);
+    // creating iframe element to fill with searched video
     var videoIframe = $("<iframe>");
-    videoIframe.attr("src",videoURL);
-    videoIframe.attr("height","316");
-    videoIframe.attr("width","560");
-    videoIframe.attr("frameborder","0");
-    videoIframe.attr("allow","accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture");
+    videoIframe.attr("src", videoURL);
+    videoIframe.attr("height", "316");
+    videoIframe.attr("width", "560");
+    videoIframe.attr("frameborder", "0");
+    videoIframe.attr("allow", "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture");
     videoIframe.attr("allowfullscreen");
     $("#videoSection").html(videoIframe);
-
-
-
-
-    
-
   })
 }
+// Click event for search button that runs function created uptop
 $("#searchBtn").on("click", function (event) {
   event.preventDefault();
   var temp = $("#search").val().trim()
-  console.log(temp)
-  console.log("yes")
-
   getVideo(temp);
-
-
-
-
 })
 
 
